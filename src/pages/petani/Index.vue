58c7fdd95fdcd4d1ed2dbaaa75c5e8fd63e88307
bellasvr <bella.16411029@student.ubl.ac.id>
2020-03-01 20:22:58 +0700

    sd

--- src/pages/petani/Index.vue
+++ src/pages/petani/Index.vue
@@ -4,13 +4,13 @@
       <q-img style="width: 40%;" src="statics/profile.png"></q-img>
     </div>
     <div class="row justify-center bg-primary">
-      <div class="text-h6 text-white">187126483423</div>
+      <div class="text-h6 text-white">{{ nik }}</div>
     </div>
     <div class="row justify-center bg-primary">
-      <div class="text-h6 text-white">Bella SVR</div>
+      <div class="text-h6 text-white">{{ nama }}</div>
     </div>
     <div class="row justify-center bg-primary q-pb-lg q-pt-md">
-      <q-btn no-caps rounded unelevated color="accent">Profile Petani</q-btn>
+      <!-- <q-btn no-caps rounded unelevated color="accent">Profile Petani</q-btn> -->
     </div>
     <div class="row justify-center q-pb-lg q-pt-md" >
       <div class="col-xs-6 q-pa-md" @click="$router.push({ name: 'Input Rut' })">
@@ -58,6 +58,17 @@
 </template>
 <script>
 export default {
+  data () {
+    return {
+      nik: '',
+      nama: ''
+    }
+  },
+  created () {
+    let dataUser = this.$q.localStorage.getItem('datauser')
+    this.nik = dataUser.nik
+    this.nama = dataUser.nama
+  },
   methods: {
     logout () {
       this.$q.localStorage.clear()
